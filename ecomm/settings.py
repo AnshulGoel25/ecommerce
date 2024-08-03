c8b33e378724e87847e13b292dcd31e6e61eb46a
Anshul Goel <anshul.goel.2534@gmail.com>
2024-08-03 16:34:21 +0530

    Removed SMTP credentials

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -146,5 +146,3 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'anshulgoel2503@gmail.com'
-EMAIL_HOST_PASSWORD = 'ften duvw ptwf pngw'
